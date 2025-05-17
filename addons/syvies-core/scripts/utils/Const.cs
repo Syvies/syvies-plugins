982ae3fcf93749b1e0687210bd10aa7f21c0e3e0
Syvies <ugo.saintandre@gmail.com>
2025-05-17 14:01:21 +0200

    Added new const + upgraded pid 3d

--- addons/syvies-core/scripts/utils/Const.cs
+++ addons/syvies-core/scripts/utils/Const.cs
@@ -4,5 +4,9 @@ namespace SyviesCore.Utils;
 
 public static class Const
 {
+	public const float F_ZERO = 0f;
+	public const int I_ZERO = 0;
+
 	public static readonly Vector2 Vec2NaN = new(float.NaN, float.NaN);
+	public static readonly Vector3 Vec3NaN = new(float.NaN, float.NaN, float.NaN);
 }
--- addons/syvies-core/scripts/utils/Extensions.cs
+++ addons/syvies-core/scripts/utils/Extensions.cs
@@ -8,4 +8,24 @@ public static class Extensions
 	{
 		return float.IsNaN(vector.X) && float.IsNaN(vector.Y);
 	}
+
+
+	public static bool IsNaN(this Vector3 vector)
+	{
+		return float.IsNaN(vector.X) && float.IsNaN(vector.Y) && float.IsNaN(vector.Z);
+	}
+
+
+	public static Transform3D NormalAligned(this Transform3D transform, Vector3 normal)
+	{
+		if (normal.IsZeroApprox()) { return transform; }
+
+		normal = normal.Normalized();
+
+		Basis aligned = transform.Basis;
+		aligned.Y = normal;
+		aligned.X = -aligned.Z.Cross(normal);
+
+		return new Transform3D(aligned, transform.Origin).Orthonormalized();
+	}
 }
